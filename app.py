4cb02f530983cacc62f174f75f8782b318b207cc
Sora <sorazora1.4@gmail.com>
2022-11-29 04:27:49 -0800

    created a user authentication method when attempting to log in

--- app.py
+++ app.py
@@ -1,28 +1,28 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, flash
 import json
 
 app = Flask(__name__)
+app.secret_key = '123'
 
 @app.route('/')
 def home():
     # saving the json contents to a variable
-    with open('./database/scores.json', 'r') as readFile:
-        score_List = json.load(readFile)
-        users = score_List.keys()
+    score_List = updateDB('./database/scores.json', 'r')
+    users = score_List.keys()
 
-        sorted_List = []
+    sorted_List = []
 
-        # sorting functionalty to sort from greatest to least in score
-        for user in users:
-            for score in score_List[user]:
-                user_Score = {
-                    "username": user,
-                    "score": score["score"],
-                    "date": score["date"]
-                }
-                sorted_List.append(user_Score)
+    # sorting functionalty to sort from greatest to least in score
+    for user in users:
+        for score in score_List[user]:
+            user_Score = {
+                "username": user,
+                "score": score["score"],
+                "date": score["date"]
+            }
+            sorted_List.append(user_Score)
 
-        sorted_List = sorted(sorted_List, key=lambda x: x["score"], reverse=True)
+    sorted_List = sorted(sorted_List, key=lambda x: x["score"], reverse=True)
 
     return render_template('home.html', sorted_List=sorted_List)
 
@@ -31,8 +31,7 @@ def submit():
     data = request.json
 
     # read the contents of the scores.json file
-    with open('./database/scores.json', 'r') as readFile:
-        score_List = json.load(readFile)
+    score_List = updateDB('./database/scores.json', 'r')
 
     # gets the list of users in the scores.json file
     user_List = score_List.keys()
@@ -57,14 +56,51 @@ def submit():
         score_List[data["username"]] = user_Score
 
     # writes the new score that was added to the database
-    with open('./database/scores.json', 'w') as writeFile:
-        json.dump(score_List, writeFile)        
+    updateDB('./database/scores.json', 'w', score_List)
 
     return redirect('/')
 
 @app.route('/login')
 def login():
     return render_template('login.html')
+
+@app.route('/login', methods=['GET', 'POST'])
+def authenticate():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+
+        users_List = updateDB('./database/users.json', 'r')
+
+        for user in users_List:
+            if user["username"] == username and user["password"] == password:
+                score_List = updateDB('./database/scores.json', 'r')
+                return render_template('login.html')
+
+            else:
+                error = 'Invalid Username or Password'
+                return render_template('login.html', error=error)
+    
+        return render_template('login.html')
+
+    if request.method == 'GET':
+        return render_template('login.html')
+
+
+# method to read or write the json file from the database
+def updateDB(database, method, updateJSON=None):
+    if method == 'r':
+        with open(database, 'r') as readFile:
+            score_List = json.load(readFile)
+            return score_List
+
+    elif method == 'w':
+        with open(database, 'w') as writeFile:
+            json.dump(updateJSON, writeFile)
+
+    else:
+        print('There was an error')
+        return None
 
 if __name__ == '__main__':
     app.run(debug=True)
