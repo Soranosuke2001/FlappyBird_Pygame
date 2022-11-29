e5e2e8ced23f1905222e236d01c1d7752867c0d0
Sora <sorazora1.4@gmail.com>
2022-11-29 14:28:04 -0800

    added doc strings to player.py

--- code/background.py
+++ code/background.py
@@ -3,14 +3,28 @@ from gamesettings import *
 
 class Background(pygame.sprite.Sprite):
     def __init__(self, groups, scaling):
+        """
+        Constructor for the Background class
+
+        Description:
+
+
+        Args:
+            groups: the sprite group to be added to
+            scaling: the scaling factor for the background image
+
+        Returns: None
+        """
         super().__init__(groups)
 
         # importing the background image and scaling it to fit the window size
         bkg_Img = pygame.image.load('../images/background/game/background.jpg').convert()
 
+        # sets the new width and height of the background
         width_Scaled = bkg_Img.get_width() * scaling
         height_Scaled = bkg_Img.get_height() * scaling
 
+        # scales the background image
         bkg_Img_Scaled = pygame.transform.scale(bkg_Img, (width_Scaled, height_Scaled))
         self.image = pygame.Surface((width_Scaled * 2, height_Scaled))
 
@@ -21,7 +35,19 @@ class Background(pygame.sprite.Sprite):
         self.rect = self.image.get_rect(topleft = (0, 0))
         self.pos = pygame.math.Vector2(self.rect.topleft)
 
+
     def update(self, delta_Time):
+        """
+        Updates the background image
+
+        Description:
+            Moves the background image from right to left continuously
+
+        Args:
+            delta_Time: the amount of time passed since the last update
+        """
+
+        # moveing the background image from left to right
         self.pos.x -= 300 * delta_Time
 
         if self.rect.centerx <= 0:
--- code/player.py
+++ code/player.py
@@ -3,6 +3,18 @@ from gamesettings import *
 
 class Player(pygame.sprite.Sprite):
     def __init__(self, groups, scaling):
+        """
+        Constructor for the Player class
+
+        Description:
+            Imports the image of kirby and masks the kirby so that the hit box is around the kirby outline
+
+        Args:
+            groups: adds the player to the sprite group
+            scaling: determines the scaling for the image of kirby
+
+        Returns: None
+        """
         super().__init__(groups)
         self.sprite_Type = 'player'
 
@@ -24,16 +36,56 @@ class Player(pygame.sprite.Sprite):
         self.jump_Sound = pygame.mixer.Sound('../music/jump.wav')
 
     def draw(self, screen_Size):
+        """
+        Draws the player sprite
+
+        Args:
+            screen_Size: size of the screen
+
+        Returns: None
+        """
         screen_Size.blit(self.image, self.rect)
 
     def player_gravity(self, delta_Time):
+        """
+        Applies gravity to the player
+
+        Description:
+            Applies gravity to the player and increases gradually as the player continuously falls down
+
+        Args:
+            delta_Time: time difference between the last frame and the current frame
+
+        Returns: None
+        """
         self.direction += self.gravity * delta_Time
         self.pos.y += self.direction * delta_Time
         self.rect.y = round(self.pos.y)
 
     def player_jump(self):
+        """
+        Jumps the player
+
+        Description:
+            Jumps the player
+
+        Args: None
+
+        Returns: None
+        """
         self.direction = -360
         self.jump_Sound.play()
 
     def update(self, delta_Time):
+        """
+        Updates the player
+
+        Description:
+            Updates the player movement
+
+        Args:
+            delta_Time: time difference between the last frame and the current frame
+
+        Returns: None
+        """
         self.player_gravity(delta_Time)
