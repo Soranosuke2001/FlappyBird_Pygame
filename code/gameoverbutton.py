e351bdd014d8264b343fb0a2cb08ce2910892ab7
Sora <sorazora1.4@gmail.com>
2022-11-29 14:40:00 -0800

    added doc strings to the gameoverbutton.py

--- code/gameoverbutton.py
+++ code/gameoverbutton.py
@@ -2,8 +2,25 @@ import pygame
 
 class GameOverButton():
     def __init__(self, x, y, image, screen, scale):
+        """
+        Constructor for the GameOverButton class
+
+        Args:
+            x: x position of the button
+            y: y position of the button
+            image: the image of the button
+            screen: the screen size of the button
+            scale: the scale factor of the image
+
+        Returns:
+            None
+        """
+
+        # gets the width and height of the image
         width = image.get_width()
         height = image.get_height()
+
+        # scales the image
         self.image = pygame.transform.scale(image, (int(width * scale), int(height * scale)))
         self.screen_Size = screen
         self.rect = self.image.get_rect()
@@ -11,6 +28,19 @@ class GameOverButton():
         self.mouse_Clicked = False
 
     def draw(self):
+        """
+        Draws the button
+
+        Description:
+            Draws the button and checks if the button was pressed or not
+
+        Args:
+            None
+
+        Returns:
+            True if the button was pressed
+            False if the button was not pressed
+        """
         # checks if the user clicked on a button
         mouse_Pos = pygame.mouse.get_pos()
         pressed = False
@@ -19,7 +49,8 @@ class GameOverButton():
         if self.rect.collidepoint(mouse_Pos):
             if pygame.mouse.get_pressed()[0] == True and self.mouse_Clicked == False:
                 self.mouse_Clicked = True
-                pressed = True            
+                pressed = True           
+
             # resets the click state
             if pygame.mouse.get_pressed()[0] == False:
                 self.mouse_Clicked = False
